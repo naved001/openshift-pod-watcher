d78dc8e9ea5f9f8b6c5e291abe0408f8a8955e76
Naved Ansari <naved001@gmail.com>
2025-11-05 11:13:16 -0500

    Allow loading config from within a pod
    This will allow us to run it on a cluster when the serviceaccount is
    bound to appropriate RBACs.

--- src/kube.py
+++ src/kube.py
@@ -1,6 +1,17 @@
 from kubernetes import client, config
-
+from .logger import get_logger
 
 def get_corev1_api():
-    config.load_kube_config()
+    logger = get_logger()
+    try:
+        config.load_incluster_config()
+        logger.info("Loaded in-cluster kubernetes config")
+    except config.ConfigException:
+        logger.info("In-cluster config not found")
+        try:
+            config.load_kube_config()
+            logger.info("Loaded config from kube context")
+        except config.ConfigException as e:
+            logger.error(f"Failed to load any kubernetes configuration: {e}")
+            raise
     return client.CoreV1Api()
