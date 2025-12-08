21c1fe10c6cab6a743e71e51e44b59e733b62f76
Naved Ansari <naved001@gmail.com>
2025-12-08 14:08:13 -0500

    Handle SIGTERM by raising keyboardinterrupt
    This is useful when kubernetes decides to kill the pod so this handles
    it gracefully and quickly.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
+import signal
+import sys
 from .config import DB_PATH
 from .db import PodDatabase
 from .kube import get_corev1_api
 from .watcher import watch_loop
 from .logger import get_logger
 
+def handle_interrupt(signal, frame):
+    raise KeyboardInterrupt
 
 def main():
     logger = get_logger()
     api = get_corev1_api()
     pod_database = PodDatabase(str(DB_PATH))
+
+    signal.signal(signal.SIGINT, handle_interrupt)
+    signal.signal(signal.SIGTERM, handle_interrupt)
 
     logger.info("Starting application!")
     try:
@@ -16,6 +23,7 @@ def main():
     except KeyboardInterrupt:
         logger.info("NOOOOOO Don't kill me please.")
         pod_database.close()
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
